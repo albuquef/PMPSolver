5c48faa6df79670838daefd94cdda1a8630d64e8
wolledav <wollerdavid@gmail.com>
2021-04-16 16:15:48 +0200

    large instances config, gitignore update

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,18 @@
 using namespace std;
 
 int main() {
-    string loc_filename = "./provided/toulon/toulon_destination.txt";
-    string cust_filename = "./provided/toulon/toulon_origin.txt";
-    string dist_filename = "./provided/toulon/toulon_orig_x_tps.txt";
+//    string loc_filename = "./provided/toulon/toulon_destination.txt";
+//    string cust_filename = "./provided/toulon/toulon_origin.txt";
+//    string dist_filename = "./provided/toulon/toulon_orig_x_tps.txt";
+
+    string loc_filename = "./provided/paca/global_destination.txt";
+    string cust_filename = "./provided/paca/global_origin.txt";
+    string dist_filename = "./provided/paca/global_orig_x_tps.txt";
 
     Instance instance(loc_filename, cust_filename, dist_filename, 5);
 
-    Instance instance1 = instance;
-
+//    Instance instance1 = instance;
     cout << instance.getDist(690, 1) << endl;
-
 
     return 0;
 }
