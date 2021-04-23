2024fe7c736cb1767a7e42e498c8a93e0fd973f0
wolledav <wollerdavid@gmail.com>
2021-04-23 11:48:59 +0200

    inefficient sampleSubvector

--- src/utils.cpp
+++ src/utils.cpp
@@ -10,24 +10,6 @@ void tock(chrono::steady_clock::time_point start) {
     cout << "Elapsed time: " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << " ms" << endl << endl;
 }
 
-vector<uint_t> sampleSubvector(vector<uint_t> *orig_vector, uint_t max_value, uint_t new_size, default_random_engine *generator) {
-    vector<bool> loc_flags(max_value + 1, false);
-    vector<uint_t> new_vector;
-
-    std::uniform_int_distribution<uint_t> distribution(0,orig_vector->size() - 1); // sample index in orig_vector
-    uint_t cand;
-    uint_t cnt = 0;
-    while (cnt < new_size) {
-        cand = orig_vector->at(distribution(*generator));
-        if (!loc_flags[cand]) {
-            cnt++;
-            loc_flags[cand] = true;
-            new_vector.push_back(cand);
-        }
-    }
-    return new_vector;
-}
-
 vector<uint_t> getRandomSubvector(vector<uint_t> orig_vector, uint_t new_size, default_random_engine *generator) {
     shuffle(orig_vector.begin(), orig_vector.end(), *generator);
     return vector<uint_t>(orig_vector.begin(), orig_vector.begin() + new_size);
