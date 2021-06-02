9f600bc6e5c30401c7682bc804c8e065f9093ca4
wolledav <wollerdavid@gmail.com>
2021-06-02 13:55:09 +0200

    current solve script

--- scripts/solve.sh
+++ scripts/solve.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 CMD=./cmake-build-debug/large_PMP
-D_MATRIX=./data/toulon/dist_matrix.txt
-WEIGHTS=./data/toulon/weights_labeled.txt
+D_MATRIX=./data/paca_v3/dist_matrix.txt
+WEIGHTS=./data/paca_v3/weights_labeled.txt
 
 for p in 37 60 174 335 352 478 681 1924
 do
