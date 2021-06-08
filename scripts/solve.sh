5699d3706d7b0880c6375af0a5663d84a6852c7e
wolledav <wollerdavid@gmail.com>
2021-06-08 10:02:14 +0200

    new solutions of PACA_v3

--- scripts/solve.sh
+++ scripts/solve.sh
@@ -4,7 +4,8 @@ CMD=./cmake-build-debug/large_PMP
 D_MATRIX=./data/paca_v3/dist_matrix.txt
 WEIGHTS=./data/paca_v3/weights_labeled.txt
 
-for p in 37 60 174 335 352 478 681 1924
+# 37 60
+for p in 174 335 352 478 681 1924
 do
-  $CMD -p $p -dm $D_MATRIX -w $WEIGHTS -o ./solutions/PACA_v3_p=${p}.txt
+  $CMD -p $p -dm $D_MATRIX -w $WEIGHTS -o ./solutions/PACA_v3_p=${p}.txt | tee console.txt
 done
