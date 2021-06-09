6c29f44211ae903a7d924937caf19613d50359e7
wolledav <wollerdavid@gmail.com>
2021-06-09 09:21:23 +0200

    globals updated to cPMP

--- src/globals.hpp
+++ src/globals.hpp
@@ -1,9 +1,11 @@
 #ifndef LARGE_PMP_GLOBALS_HPP
 #define LARGE_PMP_GLOBALS_HPP
-
 #define TOLERANCE 0.000001
 #define DEFAULT_DISTANCE 1000000000
 #define DEFAULT_WEIGHT 0
+#define DEFAULT_CAPACITY 0
+
+#include <vector>
 
 using namespace std;
 
@@ -12,6 +14,12 @@ typedef double dist_t;
 typedef struct {uint_t node;
                 dist_t dist;
                 } my_pair;
+typedef struct {
+    uint_t node;
+    uint_t usage;
+    dist_t dist;
+} my_tuple;
+typedef vector<my_tuple> assignment;
 
 extern uint_t LOC_FREQUENCY; // avg. no. of location occurrences in M sub-PMPs (M = LOC_FREQUENCY*N/n)
 extern double BW_CUTOFF; // locations within BW_CUTOFF * h from a selected one obtain nonzero voting score
