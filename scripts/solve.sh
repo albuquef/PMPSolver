49983321fe4ea2f3eddd885601537c09742ad60f
wolledav <wollerdavid@gmail.com>
2021-06-17 11:26:39 +0200

    solve script updated

--- scripts/solve.sh
+++ scripts/solve.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 CMD=./cmake-build-debug/large_PMP
-D_MATRIX=./data/paca_v4/dist_matrix.txt
-WEIGHTS=./data/paca_v4/cust_weights.txt
-CAPACITIES=./data/paca_v4/capacities_col.txt
+D_MATRIX=./data/paca_v3/dist_matrix.txt
+WEIGHTS=./data/paca_v3/cust_weights.txt
+CAPACITIES=./data/paca_v3/capacities_col.txt
 
 for p in 37 60 174 335 352 478 681 1924
 do
-  $CMD -p $p -dm $D_MATRIX -w $WEIGHTS -c $CAPACITIES -mode 3 -o ./solutions/PACA_v4_p=${p}.txt | tee console.txt
+  $CMD -p $p -dm $D_MATRIX -w $WEIGHTS -c $CAPACITIES -mode 3 -o ./solutions/paca_v3_p=${p}.txt | tee console_${p}.txt
 done
 
 
