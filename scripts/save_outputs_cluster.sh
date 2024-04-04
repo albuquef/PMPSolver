22d9e718ed3cb7bc9149fee39b4c7b7ae6442dca
albuquef <felipe.albuquerquebs@gmail.com>
2024-04-04 17:28:32 +0200

    save outputs scripts

---
+++ scripts/save_outputs_cluster.sh
@@ -0,0 +1,10 @@
+# Path: scripts/send_code_to_cluster.sh
+# Compare this snippet from scripts/send_code_to_cluster.sh:
+PATH_CLUSTER=falbuquerque@poseidon:/users/falbuquerque/Projects/Project_PMP/PACA_cyrille/originial_instance/cpmp/test_exact/
+PATH_LOCAL=//home/falbuquerque/Documents/projects/Project_PMP/Cluster_tests_saved/PACA_cyrille/original_instance/CPMP/test_exats_complement/
+
+# Save the outputs to the local machine
+eval "scp -r $PATH_CLUSTER/slurm* $PATH_LOCAL"
+eval "scp -r $PATH_CLUSTER/solutions/ $PATH_LOCAL"
+eval "scp -r $PATH_CLUSTER/reports/ $PATH_LOCAL"
+eval "scp -r $PATH_CLUSTER/console/ $PATH_LOCAL"
