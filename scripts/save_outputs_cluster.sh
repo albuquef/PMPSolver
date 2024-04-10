ff22da26af3fb6d5e8682443fbe189045980605c
felipeealb <felipe.albuquerquebs@gmail.com>
2024-04-11 01:50:48 +0200

    update tables

--- scripts/save_outputs_cluster.sh
+++ scripts/save_outputs_cluster.sh
@@ -3,8 +3,8 @@
 # PATH_CLUSTER=falbuquerque@poseidon:/users/falbuquerque/Projects/Project_PMP/PACA_cyrille/originial_instance/cpmp/test_exact/
 PATH_CLUSTER=falbuquerque@poseidon:/users/falbuquerque/Projects/Project_PMP/PACA_cyrille/originial_instance/cpmp_cover/test_coverages_exact/
 # PATH_LOCAL=//home/falbuquerque/Documents/projects/Project_PMP/Cluster_tests_saved/PACA_cyrille/original_instance/CPMP/test_exats_complement/
-# PATH_LOCAL=/home/felipe/Documents/Projects/GeoAvigon/save_cluster/test_exact/
-PATH_LOCAL=//home/falbuquerque/Documents/projects/Project_PMP/Cluster_tests_saved/PACA_cyrille/original_instance/CPMP_cover/test_coverages_exact/
+PATH_LOCAL=/home/felipe/Documents/Projects/GeoAvigon/save_cluster/test_coverages_exact/
+# PATH_LOCAL=//home/falbuquerque/Documents/projects/Project_PMP/Cluster_tests_saved/PACA_cyrille/original_instance/CPMP_cover/test_coverages_exact/
 
 
 # Save the outputs to the local machine
