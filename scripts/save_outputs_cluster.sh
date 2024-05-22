5667d23218abb9ad1bfb1412664906cd124aff79
albuquef <felipe.albuquerquebs@gmail.com>
2024-05-22 19:53:45 +0200

    updates

--- scripts/save_outputs_cluster.sh
+++ scripts/save_outputs_cluster.sh
@@ -6,12 +6,10 @@ DATE=$(date +"%y-%m-%d")
 # TYPEINSTANCE=Random_Grid
 TYPEINSTANCE=PACA_instance
 
-
-
-PATH_CLUSTER=falbuquerque@poseidon:/users/falbuquerque/Projects/Project_PMP/PACA_cyrille/originial_instance/cpmp/test_exact/
+PATH_CLUSTER=falbuquerque@poseidon:/users/falbuquerque/Projects/Project_PMP/test_kmeans/literature
 # PATH_CLUSTER=falbuquerque@poseidon:/users/falbuquerque/Projects/Project_PMP/${TYPEINSTANCE}/
 # PATH_LOCAL=/home/falbuquerque/Documents/projects/save_old_test_cluster/${DATE}_save_cluster/${TYPEINSTANCE}/
-PATH_LOCAL=/home/felipe/Documents/doctorat_avignon/project_pmp/save_cluster/${DATE}_save-cluster/${TYPEINSTANCE}/test_exact/
+PATH_LOCAL=/home/falbuquerque/Documents/projects/Project_PMP/saves/SaveCluster/savecluster_Literature/24-05-22_CPMP_cover_kmeans_literature_exact_savecluster/
 
 
 # Save the outputs to the local machine
