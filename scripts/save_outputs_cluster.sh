55fa1b01dd511f06b0544caf0ee325aa2afc3e0a
felipeealb <felipe.albuquerquebs@gmail.com>
2024-05-24 08:33:20 +0200

    updates

--- scripts/save_outputs_cluster.sh
+++ scripts/save_outputs_cluster.sh
@@ -6,14 +6,15 @@ DATE=$(date +"%y-%m-%d")
 # TYPEINSTANCE=Random_Grid
 TYPEINSTANCE=PACA_instance
 
-PATH_CLUSTER=falbuquerque@poseidon:/users/falbuquerque/Projects/Project_PMP/test_kmeans/literature
+PATH_CLUSTER=falbuquerque@poseidon:/users/falbuquerque/Projects/Project_PMP/test_all_coverages/
+# PATH_CLUSTER=falbuquerque@poseidon:/users/falbuquerque/Projects/Project_PMP/PACA_cyrille/originial_instance/test_all_coverages/
 # PATH_CLUSTER=falbuquerque@poseidon:/users/falbuquerque/Projects/Project_PMP/${TYPEINSTANCE}/
 # PATH_LOCAL=/home/falbuquerque/Documents/projects/save_old_test_cluster/${DATE}_save_cluster/${TYPEINSTANCE}/
-PATH_LOCAL=/home/falbuquerque/Documents/projects/Project_PMP/saves/SaveCluster/savecluster_Literature/24-05-22_CPMP_cover_kmeans_literature_exact_savecluster/
+PATH_LOCAL=/home/felipe/Documents/save_cluster/test_all_coverages/
 
 
 # Save the outputs to the local machine
-eval "scp -r $PATH_CLUSTER/slurm* $PATH_LOCAL"
+# eval "scp -r $PATH_CLUSTER/slurm* $PATH_LOCAL"
 eval "scp -r $PATH_CLUSTER/solutions/ $PATH_LOCAL"
 eval "scp -r $PATH_CLUSTER/reports/ $PATH_LOCAL"
-eval "scp -r $PATH_CLUSTER/console/ $PATH_LOCAL"
+# eval "scp -r $PATH_CLUSTER/console/ $PATH_LOCAL"
